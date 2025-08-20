84dbe68cc57dd0e5535aa1d57731fc6e4fad4f70
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-08-20 19:54:36 -0300

    feat: create User.ts

---
+++ backend/src/models/User.ts
@@ -0,0 +1,52 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column() 
+    name: string;
+
+    @Column() 
+    password: string;
+
+    @Column({ type: 'varchar', length: 20, nullable: true, default: null })
+    phone: string | null;
+
+    @Column({
+        type: 'enum',
+        enum: ['male', 'female', 'preferNotToSay', 'other']
+    })
+    gender: string;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @Column()
+    birthday: Date;
+
+    constructor(
+        name: string,
+        email: string,
+        password: string,
+        birthday: Date,
+        phone: string,
+        gender: string
+    ) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone ?? null;
+        this.gender = gender ?? 'preferNotToSay';
+        this.birthday = birthday;
+    }
+}

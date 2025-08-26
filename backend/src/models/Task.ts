67adee0f43b8575b39fef8196cbaa1b67743ccc0
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-08-25 21:40:04 -0300

    feat: create Task.ts

---
+++ backend/src/models/Task.ts
@@ -0,0 +1,20 @@
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./User";
+import { Group } from "./Groups";
+
+@Entity("tasks") 
+export class Task{
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @ManyToOne(() => User, user => user.tasks, { onDelete: "CASCADE"})
+    user: User;
+
+    @ManyToOne(() => Group, group => group.tasks)
+    group: Group
+
+    constructor(user: User, group: Group) {
+        this.user = user
+        this.group = group
+    }
+}

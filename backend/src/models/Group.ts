79af5b5fa8fbee86be7b2e2914b7360ea33b1bd7
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-08-25 21:41:01 -0300

    feat: create Group.ts

---
+++ backend/src/models/Group.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Task } from "./Task";
+
+@Entity("groups") 
+export class Group {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @OneToMany(() => Task, task => task.group)
+    tasks: Task[];
+
+    @Column("json")
+    days: number[];
+
+    constructor(days: number[],tasks: Task[]) {
+        this.tasks = tasks ?? []
+        this.days = days ?? [0,1,2,3,4,5,6]
+    }
+}

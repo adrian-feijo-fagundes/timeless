7d66eb3ff3596d7b6e2e5f5534d3564be908076b
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-08-27 19:43:23 -0300

    feat: update User.ts to use bcrypt

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,9 +1,13 @@
+import * as bcrypt from "bcryptjs"; // Importando o bcryptjs para o hash da senha
 import {
     Entity,
     PrimaryGeneratedColumn,
     Column,
     CreateDateColumn,
     OneToMany,
+    BeforeInsert,
+    BeforeUpdate,
+    AfterLoad,
 } from "typeorm";
 import { Task } from "./Task";
 
@@ -40,6 +44,25 @@ export class User {
     @OneToMany(() => Task, task => task.user)
     tasks?: Task[];
 
+
+    private previousPassword!: string;
+
+    @BeforeInsert() // Antes de inserir um novo usuário
+    @BeforeUpdate() // Antes de atualizar um usuário existente
+    async hashPassword() {
+        // Verifica se a senha foi alterada, para não fazer hash desnecessariamente
+        if (this.password && this.password !== this.previousPassword) {
+            const salt = await bcrypt.genSalt(10); // Gera um 'salt' com 10 rounds, o salt é uma string aleatória que irá aumentar a segurança do hash
+            this.password = await bcrypt.hash(this.password, salt); // Faz o hash da senha com o salt gerado
+        }
+    }
+
+    // Hook AfterLoad: Este hook é chamado depois de carregar o usuário do banco
+    @AfterLoad()
+    setPreviousPassword() {
+        // Aqui estamos salvando a senha original para comparar em futuras atualizações
+        this.previousPassword = this.password;
+    }
 
     constructor(
         name: string,

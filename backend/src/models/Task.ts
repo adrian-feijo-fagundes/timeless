d5cec28e1905fe261465c9735dcf889cc5ee5b50
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-09-01 18:45:11 -0300

    feat(task): create some task atributtes

--- backend/src/models/Task.ts
+++ backend/src/models/Task.ts
@@ -1,24 +1,48 @@
-import { Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./User";
 import { Group } from "./Group";
-import { Appointment } from "./Appointment";
 
 @Entity("tasks") 
 export class Task{
     @PrimaryGeneratedColumn()
     id!: number;
 
+    @Column()
+    title: string;
+
+    @Column()
+    topic: string;
+
+    @Column()
+    status: string;
+
+    @Column()
+    frequent: boolean;
+
+    @Column({ nullable: true})
+    limitDate: Date | null;
+
     @ManyToOne(() => User, user => user.tasks, { onDelete: "CASCADE"})
     user: User;
 
     @ManyToOne(() => Group, group => group.tasks)
     group: Group;
-
-    @OneToOne(() => Appointment, appointment => appointment.task)
-    appointment?: Appointment
     
-    constructor(user: User, group: Group) {
+    constructor(
+        user: User, 
+        group: Group,
+        title: string,
+        topic: string,
+        status: string,
+        frequent: boolean,
+        limitDate: Date,
+    ) {
         this.user = user
         this.group = group
+        this.title = title
+        this.topic = topic
+        this.status = status
+        this.frequent = frequent
+        this.limitDate = limitDate
     }
 }

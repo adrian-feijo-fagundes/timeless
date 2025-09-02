6b5d0835f19fae74672210a45fbef42ebe0297eb
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-09-01 21:36:52 -0300

    feat: integrar rotas de tarefas ao sistema principal
    - Adicionar taskRoutes ao router principal
    - Manter compatibilidade com rotas existentes
    - Organizar estrutura completa de rotas do sistema

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import userRoutes from "./userRouter";
 import groupRoutes from "./groupRouter";
+import taskRoutes from "./taskRouter";
 
 const routes = Router();
 
 routes.use(userRoutes);
 routes.use(groupRoutes);
+routes.use(taskRoutes);
 
 export default routes;

aa736b370ec9ead94bef6330adcd22c289eb58b6
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-09-01 21:17:30 -0300

    feat: integrar rotas de grupos ao sistema principal
    - Adicionar groupRoutes ao router principal
    - Manter compatibilidade com rotas existentes
    - Organizar estrutura de rotas do sistema

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import userRoutes from "./userRouter";
+import groupRoutes from "./groupRouter";
 
 const routes = Router();
 
-routes.use(userRoutes)
+routes.use(userRoutes);
+routes.use(groupRoutes);
 
 export default routes;

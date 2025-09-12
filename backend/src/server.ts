3b69df49217436b9b81a351ee79030bfa70a7f82
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-09-12 19:41:09 -0300

    chore: add cors.js and comment cron temporarily

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,5 @@
 import express, { Application } from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import routes from "./routes";
 import dotenv from "dotenv";
 import { AppDataSource } from "./config/dataSource";
@@ -11,25 +10,25 @@ dotenv.config()
 const PORT = Number(process.env.SERVER_PORT) || 3000
 const app: Application = express();
 
-app.use(cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-    credentials: true
-}))
-
-app.use(cookieParser())
 
 AppDataSource.initialize()
     .then(() => {
 
         app.use(express.json())
 
+        app.use(cors({
+            origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+            credentials: true
+        }))
+
+
         app.use(routes)
         app.listen(PORT, () => {
             console.log('Server running in port', PORT)
             
             // Inicia o serviço de cron
-            const cronService = CronService.getInstance();
-            cronService.startConsoleLogJob();
+            //const cronService = CronService.getInstance();
+            //cronService.startConsoleLogJob();
         })
     })
     .catch(error => console.log(error))

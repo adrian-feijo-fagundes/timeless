17dbdbea90c7415098d68a50ebe6eca1836a89bc
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-11-14 15:13:33 -0300

    start TaskService implementation

---
+++ src/services/TaskService.ts
@@ -0,0 +1,8 @@
+export class TaskService {
+    create = async () => { }
+    update = async () => { }
+    getByUser = async () => { } 
+    delete =  async () => { }
+    getByGroup = async () => { }
+    getById = async () => { }
+}

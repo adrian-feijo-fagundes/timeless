9693dba05d0945f81ff33a1f31b0716e7432daab
gukisz <126225075+Gukisz@users.noreply.github.com>
2025-12-18 20:02:24 -0300

    FIX: synchronize is changed to false instead of true

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -12,6 +12,6 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: ["src/models/*.ts"],
-  synchronize: true,
+  synchronize: false,
   logging: true,
 });
